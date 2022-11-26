0b920a6a07b9ea2173e6040b529c03110a6baea6
AlonsoBarajas <barjasalonsoc@gmail.com>
2022-11-25 18:14:58 -0600

    Camera class added

--- computervision.py
+++ computervision.py
@@ -1,14 +1,36 @@
 import numpy as np
 import cv2 as cv
 
-cap = cv.VideoCapture(0)
+class Camera (cv.VideoCapture):
+    """Camera handler class"""
 
-if not cap.isOpened():
-    print("Could not access camera")
-    exit()
+    def __init__(self, cam = 0):
+        
+        # Call the init function of cv.VideoCapture
+        super(Camera, self).__init__(cam)
 
-while (cv.waitKey(1) != 27):
-    ret, frame = cap.read()
-    
-    if ret:
-        cv.imshow("Frame",frame)
+        # Assert that the camera has been opened
+        if not self.isOpened():
+
+            try:
+                raise AssertionError("Camera was not found in the given index")
+            except AssertionError as e:
+                print("Assertion error: ", e)
+
+                
+                
+
+                
+# Debug for testing the cv pipeline
+if __name__ == "__main__":
+    cap = Camera()
+
+    if not cap.isOpened():
+        print("Could not access camera")
+        exit()
+
+    while (cv.waitKey(1) != 27):
+        ret, frame = cap.read()
+        
+        if ret:
+            cv.imshow("Frame",frame)
