de7569f9d23ea6b9befb301259f60681712fe0dd
AlonsoBarajas <barajasalonsoc@gmail.com>
2022-11-29 13:31:27 -0600

    Morphology calibration p2

--- src/calibration.py
+++ src/calibration.py
@@ -24,10 +24,17 @@ if __name__ == "__main__":
     # Window and trackbar creation for parametrizing morphological operations
     morph_win = "Morphological operations"
     cv.namedWindow(morph_win)
-    cv.createTrackbar("Opening s.e. size",morph_win, 0, 10, nothing)
-    cv.createTrackbar("Opening iterations",morph_win, 0, 10, nothing)
-    cv.createTrackbar("Closing s.e. size",morph_win, 0, 10, nothing)
-    cv.createTrackbar("Closing iterations",morph_win, 0, 10, nothing)
+    cv.createTrackbar("Opening size",morph_win, 5, 10, nothing)
+    cv.createTrackbar("Opening iterations",morph_win, 1, 5, nothing)
+    cv.createTrackbar("Closing size",morph_win, 5, 10, nothing)
+    cv.createTrackbar("Closing iterations",morph_win, 1, 5, nothing)
+
+    # Morphological functions do not accept 0 values
+    cv.setTrackbarMin("Opening size", morph_win, 1)
+    cv.setTrackbarMin("Opening iterations",morph_win, 1)
+    cv.setTrackbarMin("Closing size",morph_win,  1)
+    cv.setTrackbarMin("Closing iterations",morph_win, 1)
+
 
     config_path = 'resources/cv.config.json'
     
@@ -40,6 +47,7 @@ if __name__ == "__main__":
     hsv_min = tuple
     hsv_max = tuple
     
+
 
     while (cv.waitKey(1) != 27):
         # Poll the current values of the trackbars and condition the signal values
@@ -54,17 +62,40 @@ if __name__ == "__main__":
         hsv_min = (h_min, s_min, v_min)
         hsv_max = (h_max, s_max, v_max)
 
+
+        # Poll the parameters for morphological operations
+        opening_sturct_elem = cv.getTrackbarPos("Opening size", morph_win)
+        opening_iters = cv.getTrackbarPos("Opening iterations", morph_win)
+        closing_sturct_elem = cv.getTrackbarPos("Closing size", morph_win)
+        closing_iters = cv.getTrackbarPos("Closing iterations", morph_win)
+
+        # Make structuring element into a tuple pair
+        opening_size = (opening_sturct_elem, opening_sturct_elem)
+        closing_size = (closing_sturct_elem, closing_sturct_elem)
+
         # Read a new video frame
         ret, frame = cap.read()
 
         if ret:
 
-            #Segment the image with the given HSV ranges
+            # Segment the image with the given HSV ranges
             hsv = cv.cvtColor(frame, cv.COLOR_BGR2HSV)
             bin = cv.inRange(hsv, hsv_min, hsv_max)
 
+            # Morphological operations
+            opening_se = cv.getStructuringElement(cv.MORPH_RECT, opening_size, (-1,-1))
+            eroded = cv.erode(bin, opening_se, iterations = opening_iters)
+            opened = cv.dilate(eroded, opening_se, iterations = opening_iters)
+
+            closing_se = cv.getStructuringElement(cv.MORPH_RECT, closing_size, (-1,-1))
+            dilated = cv.dilate(opened, closing_se, iterations = closing_iters)
+            closed = cv.erode(dilated, closing_se, iterations = closing_iters)
+            
             # Display the result of the callibration
+
             cv.imshow(hsv_win, bin)
+            cv.imshow(morph_win, closed)
+
     
     # Convert tuple to string
     separator = ','
@@ -74,7 +105,12 @@ if __name__ == "__main__":
     # Update the new HSV range in the JSON object
     config["detection"][0]["HSVmin"] = hsv_min_val
     config["detection"][0]["HSVmax"] = hsv_max_val
-    
+    config["detection"][0]["filters"][1]["size"] = str(opening_sturct_elem)
+    config["detection"][0]["filters"][1]["iters"] =str(opening_iters)
+    config["detection"][0]["filters"][2]["size"] = str(closing_sturct_elem)
+    config["detection"][0]["filters"][2]["iters"] = str(closing_iters)
+
+
     # Write the calibration data to the config.json file
     with open(config_path, 'w') as f:
         json.dump(config, f, indent = 4)
